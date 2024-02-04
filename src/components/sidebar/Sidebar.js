b6ae03156ed9c3cf2d51ef08f5cd04604980660a
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2024-02-04 11:24:17 -0600

    more changes

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Sidebar.css'
+import { Avatar, IconButton } from '@mui/material'
+import ChatIcon from '@mui/icons-material/Chat';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { SearchOutlined } from '@mui/icons-material';
+import SidebarChat from '../sidebarChat/SidebarChat';
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <div className='sidebar__header'>
+          <Avatar />
+          <div className='sidebar_headerRight'>
+            <IconButton>
+              <DonutLargeIcon />
+            </IconButton>
+            <IconButton>
+              <ChatIcon />
+            </IconButton>
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          </div>
+        </div>
+        <div className='sidebar__search'>
+          <div className='sidebar__searchContainer'>
+            <SearchOutlined />
+            <input placeholder='Search or start new chat' type='text'/>
+          </div>
+        </div>
+        <div className='sidebar__chats'>
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />      
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDaDArFJ0txlG7YGnja2pQ5mESTBiLMy5w",
+    authDomain: "whats-app-clone-9456c.firebaseapp.com",
+    projectId: "whats-app-clone-9456c",
+    storageBucket: "whats-app-clone-9456c.appspot.com",
+    messagingSenderId: "613126842261",
+    appId: "1:613126842261:web:9518869e1dd66f329cb89c",
+    measurementId: "G-QSYD3CHL6Y"
+  };
